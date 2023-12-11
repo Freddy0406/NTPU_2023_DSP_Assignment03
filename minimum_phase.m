e80347190b63ef599faed1c5b3cd77848014cbff
Freddy0406 <creeperqaqoao@gmail.com>
2023-12-11 13:28:05 +0800

    minimum phase完成

--- minimum_phase.m
+++ minimum_phase.m
@@ -32,30 +32,136 @@ for i = 1:length(zeros_M_64)
         zeros_M_64(i) = 1/conj(zeros_M_64(i));
     end
 end
-%% 
-MP_M_4 = fliplr(poly(zeros_M_4));
-MP_M_16 = fliplr(poly(zeros_M_16));
-MP_M_64 = fliplr(poly(zeros_M_64));
-
+%%  Calculate minimum phase(MP)
+MP_M_4 = poly(zeros_M_4);
+MP_M_16 = poly(zeros_M_16);
+MP_M_64 = poly(zeros_M_64);
 
 [data_3500hz_M4] = through_new_LPF(sin3500Hz,MP_M_4);
 [data_3500hz_M16] = through_new_LPF(sin3500Hz,MP_M_16);
 [data_3500hz_M64] = through_new_LPF(sin3500Hz,MP_M_64);
 
+data_3500hz_M4 = data_3500hz_M4./30;
+data_3500hz_M16 = data_3500hz_M16./300;
+data_3500hz_M64 = data_3500hz_M64./3000;
 
-data_3500hz_M4 = rescale(data_3500hz_M4,-1,1);
-data_3500hz_M16 = rescale(data_3500hz_M16,-1,1);
-data_3500hz_M64 = rescale(data_3500hz_M64,-1,1);
-audiowrite('3500hz_MP_M4.wav',data_3500hz_M4,fs);
-audiowrite('3500hz_MP_M16.wav',data_3500hz_M16,fs);
-audiowrite('3500hz_MP_M64.wav',data_3500hz_M64,fs);
+[data_5000hz_M4] = through_new_LPF(sin5000Hz,MP_M_4);
+[data_5000hz_M16] = through_new_LPF(sin5000Hz,MP_M_16);
+[data_5000hz_M64] = through_new_LPF(sin5000Hz,MP_M_64);
+
+data_5000hz_M4 = data_5000hz_M4./30;
+data_5000hz_M16 = data_5000hz_M16./300;
+data_5000hz_M64 = data_5000hz_M64./3000;
+
+
+sin3500Hz_M4 = load('sin3500Hz_M4.txt');
+sin3500Hz_M16 = load('sin3500Hz_M16.txt');
+sin3500Hz_M64 = load('sin3500Hz_M64.txt');
+sin5000Hz_M4 = load('sin5000Hz_M4.txt');
+sin5000Hz_M16 = load('sin5000Hz_M16.txt');
+sin5000Hz_M64 = load('sin5000Hz_M64.txt');
+
+%% plot f = 3500hz
+
+figure(1);
+subplot(4,1,1)
+plot(sin3500Hz_M4);
+title('f = 3500Hz, M = 4');
+xlim([44100 45100]);
+xlabel('Data points')
+ylabel('Amplitude')
+
+subplot(4,1,2)
+plot(data_3500hz_M4);
+title('f = 3500Hz, M = 4 (Minimum phase)');
+xlim([44100 45100])
+xlabel('Data points')
+ylabel('Amplitude')
+
+subplot(4,1,3)
+plot(sin3500Hz_M16);
+title('f = 3500Hz, M = 16');
+xlim([44100 45100])
+xlabel('Data points')
+ylabel('Amplitude')
+
+subplot(4,1,4)
+plot(data_3500hz_M16);
+title('f = 3500Hz, M = 16 (Minimum phase)');
+xlim([44100 45100])
+xlabel('Data points')
+ylabel('Amplitude')
+sgtitle('Observe transient state, f = 3500Hz, M = 4,16');
+
+figure(2);
+subplot(2,1,1)
+plot(sin3500Hz_M64);
+title('f = 3500Hz, M = 64');
+xlim([44100 45100]);
+xlabel('Data points')
+ylabel('Amplitude')
+
+subplot(2,1,2)
+plot(data_3500hz_M64);
+title('f = 3500Hz, M = 64 (Minimum phase)');
+xlim([44100 45100])
+xlabel('Data points')
+ylabel('Amplitude')
+sgtitle('Observe transient state, f = 3500Hz, M = 64');
+
+%% plot f = 5000hz
+
+figure(3);
+subplot(4,1,1)
+plot(sin5000Hz_M4);
+title('f = 5000Hz, M = 4');
+xlim([44100 45100]);
+xlabel('Data points')
+ylabel('Amplitude')
+
+subplot(4,1,2)
+plot(data_5000hz_M4);
+title('f = 5000Hz, M = 4 (Minimum phase)');
+xlim([44100 45100])
+xlabel('Data points')
+ylabel('Amplitude')
+
+subplot(4,1,3)
+plot(sin5000Hz_M16);
+title('f = 5000Hz, M = 16');
+xlim([44100 45100])
+xlabel('Data points')
+ylabel('Amplitude')
+
+subplot(4,1,4)
+plot(data_5000hz_M16);
+title('f = 5000Hz, M = 16 (Minimum phase)');
+xlim([44100 45100])
+xlabel('Data points')
+ylabel('Amplitude')
+sgtitle('Observe transient state, f = 5000Hz, M = 4,16');
+
+figure(4);
+subplot(2,1,1)
+title('123');
+plot(sin5000Hz_M64);
+title('f = 5000Hz, M = 64');
+xlim([44100 45100]);
+xlabel('Data points')
+ylabel('Amplitude')
+
+subplot(2,1,2)
+plot(data_5000hz_M64);
+title('f = 5000Hz, M = 64 (Minimum phase)');
+xlim([44100 45100])
+xlabel('Data points')
+ylabel('Amplitude')
+sgtitle('Observe transient state, f = 5000Hz, M = 64');
 
 
 
 
-
-
-%% function
+%% Function
 function [output] = through_new_LPF(data,channel)
     output = zeros(1,length(data));
     for n = 1:length(data)
